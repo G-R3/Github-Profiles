c6b6a5afe9e751b6f8b894f24f78d7c6e065e897
Gerardo <gr261212@gmail.com>
2022-01-10 17:07:44 -0500

    updated api calls

--- src/api/api.js
+++ src/api/api.js
@@ -14,16 +14,33 @@ const getUserRepos = async (user) => {
     let response = await fetch(`https://api.github.com/users/${user}/repos`);
     let data = await response.json();
 
-    let repos = data.map((repo) => ({
+    let repos = data.map((repo) => repo.name);
+
+    return getRepoDetails(user, repos);
+};
+
+const getRepoDetails = async (user, repos) => {
+    let promises = [];
+
+    for (let i = 0; i < repos.length; i++) {
+        promises.push(
+            fetch(`https://api.github.com/repos/${user}/${repos[i]}`),
+        );
+    }
+
+    let data = await Promise.all(promises).then((responses) => {
+        return Promise.all(responses.map((res) => res.json()));
+    });
+
+    return data.map((repo) => ({
         id: repo.id,
         name: repo.name,
-        url: repo.html_url,
-        stargazer_count: repo.stargazers_count,
-        watchers_count: repo.watchers_count,
+        description: repo.description,
+        fork_count: repo.forks_count,
+        stargazers_count: repo.stargazers_count,
+        subscribers_count: repo.subscribers_count,
         topics: repo.topics,
     }));
-
-    return repos;
 };
 
 export { getUser, getUserRepos };
