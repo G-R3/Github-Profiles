556338279697a33dcb1d4577647eaf20633159b8
Gerardo <gr261212@gmail.com>
2022-01-11 17:49:59 -0500

    using github search endpoint to grab repo details

--- src/api/api.js
+++ src/api/api.js
@@ -11,36 +11,37 @@ const getUser = async (query) => {
 };
 
 const getUserRepos = async (user) => {
-    let response = await fetch(`https://api.github.com/users/${user}/repos`);
+    let response = await fetch(
+        `https://api.github.com/search/repositories?q=user:${user}+sort:stars&per_page=6`,
+    );
     let data = await response.json();
 
-    let repos = data.map((repo) => repo.name);
-
-    return getRepoDetails(user, repos);
-};
-
-const getRepoDetails = async (user, repos) => {
-    let promises = [];
-
-    for (let i = 0; i < repos.length; i++) {
-        promises.push(
-            fetch(`https://api.github.com/repos/${user}/${repos[i]}`),
-        );
-    }
-
-    let data = await Promise.all(promises).then((responses) => {
-        return Promise.all(responses.map((res) => res.json()));
-    });
-
-    return data.map((repo) => ({
+    let repos = data.items.map((repo) => ({
         id: repo.id,
         name: repo.name,
         description: repo.description,
         fork_count: repo.forks_count,
         stargazers_count: repo.stargazers_count,
-        subscribers_count: repo.subscribers_count,
         topics: repo.topics,
     }));
+
+    return repos;
 };
+
+// const getRepoDetails = async (user, repos) => {
+//     let promises = [];
+
+//     for (let i = 0; i < repos.length; i++) {
+//         promises.push(
+//             fetch(`https://api.github.com/repos/${user}/${repos[i]}`),
+//         );
+//     }
+
+//     let data = await Promise.all(promises).then((responses) => {
+//         return Promise.all(responses.map((res) => res.json()));
+//     });
+
+//     return data.map((repo) => ({}));
+// };
 
 export { getUser, getUserRepos };
