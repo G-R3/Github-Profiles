ef1ef6862f0c735269562d71fc481734a41d93fc
Gerardo <gr261212@gmail.com>
2022-01-11 19:21:31 -0500

    fetching new user data

--- src/api/api.js
+++ src/api/api.js
@@ -2,11 +2,18 @@ const getUser = async (query) => {
     let response = await fetch(`  https://api.github.com/users/${query}`);
     let data = await response.json();
 
+    let [followers, following] = await getFollows(query);
+
     return {
         id: data.id,
-        username: data.login,
+        login: data.login,
+        name: data.name,
         avatar: data.avatar_url,
         page: data.html_url,
+        createdAt: data.created_at,
+        publicRepos: data.publc_repos,
+        followers,
+        following,
     };
 };
 
@@ -23,9 +30,23 @@ const getUserRepos = async (user) => {
         fork_count: repo.forks_count,
         stargazers_count: repo.stargazers_count,
         topics: repo.topics,
+        language: repo.language,
     }));
 
     return repos;
+};
+
+const getFollows = async (user) => {
+    let promises = [
+        fetch(`https://api.github.com/users/${user}/followers`),
+        fetch(`https://api.github.com/users/${user}/following`),
+    ];
+
+    let [followers, following] = await Promise.all(promises).then((res) => {
+        return Promise.all(res.map((res) => res.json()));
+    });
+
+    return [followers.length, following.length];
 };
 
 // const getRepoDetails = async (user, repos) => {
--- src/components/User.js
+++ src/components/User.js
@@ -15,7 +15,14 @@ export default function User({ user }) {
                     className="rounded-full w-60 shadow-md group-hover:shadow-lg transition-shadow"
                 />
             </a>
-            <h1 className="text-xl font-semibold mt-2">{user.username}</h1>
+            <h1 className="text-xl font-semibold mt-2">{user.name}</h1>
+            <span>{user.login}</span>
+            <span>
+                Join on: {new Date(`${user.createdAt}`).toLocaleDateString()}
+            </span>
+            <span>
+                Followers: {user.followers} | Following: {user.following}
+            </span>
         </div>
     );
 }
